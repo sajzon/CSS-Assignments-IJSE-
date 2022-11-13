9bca392c463e132419cde4e28f9bcab9a4fcc3d0
sajzon <sajithsandakelum@gmail.com>
2022-11-13 21:40:31 +0530

    JS Assignment-Que

---
+++ JS Assignment/assets/JS/script.js
@@ -0,0 +1,21 @@
+var boxId = [$('#box1'),$('#box2'),$('#box3'),$('#box4'),$('#box5'),$('#box6')];
+var color = ["beige", "lightpink", "red", "green", "blue", "yellow"];
+
+setInterval(assignColor,"500");
+
+function mixArray() {
+    var tempColor=new Array();
+    tempColor.push(color[5]);
+    for (let i = 0; i < boxId.length-1; i++) {
+        tempColor.push(color[i]);
+    }
+    color = tempColor;
+}
+
+function assignColor() {
+    mixArray();
+    for (let index = 0; index < boxId.length; index++) {
+        boxId[index].css('background-color', color[index]);
+        console.log("Das");
+    }
+}
